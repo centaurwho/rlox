43c0ace2d4df05743037a89f9f24c8b578deed68
Hakan Sonmez <hakan.sonmez@englishcentral.com>
2022-06-27 17:19:56 +0300

    Define token type and initial scanner structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use std::{env, fs, io};
+use std::io::{BufRead, Write};
+
+mod scanner;
+mod token;
+
+// TODO: error handling
+fn run(source: String) {
+    let scanner = scanner::Scanner::new(source);
+    let tokens = scanner.scan_tokens();
+    for token in tokens {
+        println!("{:?}", token)
+    }
+}
+
+fn repl() {
+    let stdin = io::stdin();
+    // TODO: Consider using a REPL crate
+    loop {
+        let mut line = String::new();
+        print!(">>> ");
+        io::stdout().flush();
+        match stdin.lock().read_line(&mut line) {
+            Ok(size) => {
+                // A way to exit REPL without resorting to CTRL+C
+                if size == 1 {
+                    break;
+                }
+            }
+            Err(_) => {}
+        }
+        run(line);
+        // println!("line: {}", line.trim().len());
+    }
+}
+
+fn run_from_file(filename: &String) {
+    let contents = fs::read_to_string(filename);
+    run(contents.unwrap());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // TODO: Graceful handling of command line values
+    // First arg is always file name
+    if args.len() == 2 {
+        run_from_file(&args[1]);
+    } else if args.len() > 2 {
+        panic!("Usage: rlox [filename]")
+    } else {
+        repl();
+    }
 }
---
+++ src/token.rs
@@ -0,0 +1,92 @@
+// TODO: Better hierarchy
+#[derive(Debug)]
+#[allow(dead_code)]
+pub enum TokenType {
+    // Single character tokens
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or Two character tokens
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals
+    Identifier,
+    String,
+    Number,
+
+    // Keywords
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    // Special
+    Eof,
+}
+
+#[derive(Debug)]
+pub enum Literal {
+    None,
+    String(String),
+    Number(u32),
+}
+
+#[derive(Debug)]
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: Literal,
+    line: u32,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, literal: Literal, line: u32) -> Token {
+        Token {
+            token_type,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_show() {
+        let t = Token::new(TokenType::Identifier, String::from("9"), Literal::Number(9), 12);
+        println!("{:?}", t);
+    }
+}
+
