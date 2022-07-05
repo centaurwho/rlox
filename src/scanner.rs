8cda899e77f7304f7f84224e0c6d2fdbd34581da
Hakan Sonmez <hakan.sonmez@englishcentral.com>
2022-07-05 14:02:34 +0300

    test 3

--- src/scanner.rs
+++ src/scanner.rs
@@ -42,6 +42,7 @@ impl Scanner {
         let c = self.advance();
         match c {
 
+
             '(' => self.add_nonliteral(TokenType::LeftParen),
             ')' => self.add_nonliteral(TokenType::RightParen),
             '{' => self.add_nonliteral(TokenType::LeftBrace),
